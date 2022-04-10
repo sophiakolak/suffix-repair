c78f202887035e0fc2e1f230a7bdb2f75ae41678
Sophia Kolak <sophiakolak.sk@gmail.com>
2022-04-10 12:56:55 -0400

    extracting lines added/deleted+verifying they match og files

--- build_db/bug_parser.py
+++ build_db/bug_parser.py
@@ -44,10 +44,32 @@ def parse_diff(patch_f, proj_name, bug):
     patch_f.close()
     return bug_diff_data
 
+#def reform_block(all_lines, in_both, changed_lines):
+#    block = ""
+#    for line in all_lines:
+#        if line in in_both:
+#            block += line[1:] #remove the added white space
+#        if line in changed_lines:
+#            block += line[1:] #remove the '-' or '+'
+#    return block
+
+def reform_block(in_both, changed_lines, total_lines):
+    block = ""
+    both_keys = list(in_both.keys())
+    changed_keys = list(changed_lines.keys())
+    all_keys = both_keys + changed_keys
+    assert(len(all_keys) == total_lines)
+    for i in sorted(all_keys):
+        if i in both_keys:
+            block += in_both[i]
+        if i in changed_keys:
+            block += changed_lines[i]
+    return block
+
+
 def extract_code(patch_f, proj_name, bug, bug_diff_data):
     if len(bug_diff_data) != 1:
         return None
-    print("PROJ: " + proj_name, "BUG: " + str(bug))
     bug_diff = bug_diff_data[0]
     old_start_line, num_old_lines = bug_diff["old_indices"].split(",")
     new_start_line, num_new_lines = bug_diff["new_indices"].split(",")
@@ -55,23 +77,23 @@ def extract_code(patch_f, proj_name, bug, bug_diff_data):
     num_old_lines = int(num_old_lines)
     new_start_line = int(new_start_line[1:])
     num_new_lines = int(num_new_lines)
-    print("old start:", old_start_line, "# old lines:", num_old_lines)
-    print("new start:", new_start_line, "# new lines:", num_new_lines)
 
     start_code = False
-    all_lines, lines_added, lines_removed, in_both = [],[],[],[]
+    all_lines, lines_added, lines_removed, in_both = {},{},{},{}
+    idx = 0
     for line in patch_f:
         if line[:2] == "@@":
             start_code = True
             continue
         if start_code:
-            all_lines.append(line)
+            all_lines[idx] = line
             if line[0] == "+":
-                lines_added.append(line)
+                lines_added[idx] = line
             elif line[0] == "-":
-                lines_removed.append(line)
+                lines_removed[idx] = line
             else: 
-                in_both.append(line)
+                in_both[idx] = line
+            idx += 1
 
     lines_in_old = len(in_both) + len(lines_removed)
     lines_in_new = len(in_both) + len(lines_added)
@@ -80,11 +102,19 @@ def extract_code(patch_f, proj_name, bug, bug_diff_data):
     assert(lines_in_old == num_old_lines) 
     assert(lines_in_new == num_new_lines)
 
+    old_snippet = reform_block(in_both, lines_removed, lines_in_old)
+    new_snippet = reform_block(in_both, lines_added, lines_in_new)
 
+    reformed_old = old_snippet.split("\n")
+    reformed_new = new_snippet.split("\n")
 
-        
+    #may be off by one due to whitespace
+    assert(len(reformed_old) == lines_in_old or len(reformed_old) == lines_in_old+1) 
+    assert(len(reformed_new) == lines_in_new or len(reformed_new) == lines_in_new+1)
 
-            
+    return old_snippet, new_snippet
+
+   
 
 def parse_info(info_f, proj_name, bug):
     bug_meta_data = {}
@@ -101,9 +131,21 @@ def aggregate(proj_name, bug, bug_meta_data, bug_diff_data):
     bug_data = {}
     bug_data["bug"] = bug
     bug_data["bug_meta_data"] = bug_meta_data
+    bug_data["bug_diff_data"] = bug_diff_data
     bug_data["proj_name"] = proj_name
     return bug_data
-        
+
+def get_proj_url(proj_name):
+    for dic in project_url:
+        if proj_name in dic:
+            return dic[proj_name]
+
+def verify_old(old_snippet, bug_data):
+    proj_name = bug_data["proj_name"]
+    bug = bug_data["bug"]
+    proj_url = get_proj_url(proj_name)
+    print("PROJ: " + proj_name, "BUG: " + str(bug), "URL: " + proj_url)
+    print(bug_data)        
 
 proj_bugs = map_proj_bug(project_tests)
 single_change_count, total = 0,0
@@ -116,8 +158,11 @@ for proj, bugs in proj_bugs.items():
             bug_meta_data = parse_info(info_f, proj, bug)
             bug_diff_data = parse_diff(patch_f, proj, bug) # this closes the file 
             patch_f = open(patch_path+"/bug_patch.txt", 'r') # reopen (change this later, it is unintuitive)
-            extract_code(patch_f, proj, bug, bug_diff_data)
+            reformed_blocks = extract_code(patch_f, proj, bug, bug_diff_data)
             bug_data = aggregate(proj, bug, bug_meta_data, bug_diff_data)
+            if reformed_blocks is not None:
+                old_snippet, new_snippet = reformed_blocks
+                verify_old(old_snippet, bug_data)
 
 
 
